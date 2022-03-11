3ebe7321ac080d0bea85906d6967593321378d98
Zayd <zayd@qumsieh.net>
2022-03-10 23:17:08 -0800

    added check_memory_unit function for type assertions

--- source/util/types.d
+++ source/util/types.d
@@ -2,31 +2,39 @@ module util.types;
 
 import util;
 
-struct Unit(T) {
+struct MemoryUnit(T) {
     T value;
     alias value this;
 
-    Unit!T opUnary(string s)() {
+    MemoryUnit!T opUnary(string s)() {
         return mixin(
-            "Unit!T(" ~ s ~ "this)"
+            "MemoryUnit!T(" ~ s ~ "this)"
         );
     }
 
-    Unit!T opBinary(string s)(Unit!T other) {
+    MemoryUnit!T opBinary(string s)(MemoryUnit!T other) {
         return mixin(
-            "Unit!T(this " ~ s ~ " other)"
+            "MemoryUnit!T(this " ~ s ~ " other)"
         );
     }
 
-    Unit!T opSlice(size_t start, size_t end) {
-        return Unit!T(this.value.bits(start, end));
+    MemoryUnit!T opSlice(size_t start, size_t end) {
+        return MemoryUnit!T(this.value.bits(start, end));
     }
 
-    Unit!T opSlice(size_t index) {
-        return Unit!T(this.value.bit(index));
+    MemoryUnit!T opSlice(size_t index) {
+        return MemoryUnit!T(this.value.bit(index));
     }
 }
 
-alias Word = Unit!uint;
-alias Half = Unit!ushort;
-alias Byte = Unit!ubyte;
+void check_memory_unit(T)() {
+    static assert (
+        is(T == Word) ||
+        is(T == Half) ||
+        is(T == Byte)
+    );
+}
+
+alias Word = MemoryUnit!uint;
+alias Half = MemoryUnit!ushort;
+alias Byte = MemoryUnit!ubyte;
