281db1db6975c1750c08715f57bfab34d5307c6f
Zayd <zayd@qumsieh.net>
2022-03-11 21:33:39 -0800

    added error variant to logging

--- source/common/log.d
+++ source/common/log.d
@@ -9,9 +9,10 @@ static immutable ulong logsource_padding = get_largest_logsource_length!();
 static ulong get_largest_logsource_length()(){
     import std.algorithm;
     import std.conv;
+    import std.traits;
 
     ulong largest_logsource_length = 0;
-    foreach (source; LogSource.min .. LogSource.max) {
+    foreach (source; EnumMembers!LogSource) {
         largest_logsource_length = max(to!string(source).length, largest_logsource_length);
     }
 
@@ -19,12 +20,15 @@ static ulong get_largest_logsource_length()(){
 }
 
 // thanks https://github.com/dlang/phobos/blob/4239ed8ebd3525206453784908f5d37c82d338ee/std/outbuffer.d
-void log(LogSource log_source, Char, A...)(scope const(Char)[] fmt, A args) {
-    import std.format.write : formattedWrite;
+private void log(LogSource log_source, bool fatal, Char, A...)(scope const(Char)[] fmt, A args) {
     import std.conv;
+    import std.format.write : formattedWrite;
+    import std.stdio;
 
-    writef("[%s: ", timestamp, pad_string_right!(to!string(log_source), logsource_padding));
+    writef("[%s] : ", pad_string_right!(to!string(log_source), logsource_padding));
     writefln(fmt, args);
+
+    if (fatal) assert(0);
 }
 
 static string pad_string_right(string s, ulong pad)() {
@@ -37,16 +41,23 @@ static string pad_string_right(string s, ulong pad)() {
 static string generate_prettier_logging_functions() {
     import std.conv;
     import std.format;
+    import std.traits;
     import std.uni;
 
     string mixed_in = "";
     
-    foreach (source; LogSource.min .. LogSource.max) {
+    foreach (source; EnumMembers!LogSource) {
         string source_name = to!string(source);
 
         mixed_in ~= "
             void log_%s(Char, A...)(scope const(Char)[] fmt, A args) {
-                log(LogSource!%s, Char, A)(fmt, args);
+                log!(LogSource.%s, false, Char, A)(fmt, args);
+            }
+        ".format(source_name.toLower(), source_name);
+
+        mixed_in ~= "
+            void error_%s(Char, A...)(scope const(Char)[] fmt, A args) {
+                log!(LogSource.%s, true, Char, A)(fmt, args);
             }
         ".format(source_name.toLower(), source_name);
     }
