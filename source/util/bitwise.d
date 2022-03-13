54cdb8936f9e6bbd9958d32d3cba88f530fb62f1
Zayd <zayd@qumsieh.net>
2022-03-12 16:34:51 -0800

    added rotate_right function to util

--- source/util/bitwise.d
+++ source/util/bitwise.d
@@ -1,14 +1,20 @@
 module util.bitwise;
 
-auto create_mask(size_t start, size_t end) {
-    return (1 << (end - start + 1)) - 1;
-}
+pragma(inline, true) {
+    auto create_mask(size_t start, size_t end) {
+        return (1 << (end - start + 1)) - 1;
+    }
 
-T bits(T)(T value, size_t start, size_t end) {
-    auto mask = create_mask(start, end);
-    return (value >> start) & mask;
-}
+    T bits(T)(T value, size_t start, size_t end) {
+        auto mask = create_mask(start, end);
+        return (value >> start) & mask;
+    }
 
-T bit(T)(T value, size_t index) {
-    return (value >> index) & 1;
+    T bit(T)(T value, size_t index) {
+        return (value >> index) & 1;
+    }
+
+    pure uint rotate_right(uint val, int shift) {
+        return (val >> shift) | (val << (32 - shift));
+    }
 }
