c9121bd58ba8c9ae34ab8b374c5623460c08dc0e
Zayd <zayd@qumsieh.net>
2022-03-19 19:03:56 -0700

    ldc, behave

--- source/util/likely.d
+++ source/util/likely.d
@@ -2,6 +2,7 @@ module util.likely;
 
 pragma(inline, true) bool likely(bool value) {
     version (LDC) {
+        import ldc.intrinsics;
         return llvm_expect!bool(value, true);
     } else {
         return value;
@@ -10,6 +11,7 @@ pragma(inline, true) bool likely(bool value) {
 
 pragma(inline, true) bool unlikely(bool value) {
     version (LDC) {
+        import ldc.intrinsics;
         return llvm_expect!bool(value, false);
     } else {
         return value;
