ba9a0a8f538df7f3f881fe77c0a9be45b0114689
GhostRain <zayd@qumsieh.net>
2022-07-31 01:28:34 -0700

    fixed rtc, finally getting secure area commands :)

--- source/emu/hw/misc/rtc.d
+++ source/emu/hw/misc/rtc.d
@@ -9,6 +9,8 @@ struct RTCParams {
     bool SCK;
     bool SIO;
     bool CS;
+
+    bool SIO_direction;
 }
 
 __gshared RTCHook rtc_hook;
@@ -34,13 +36,14 @@ final class RTCHook {
         // log_rtc("arm7: %x", arm7.regs[pc]);
         // TODO: do i need to implement the data direction bit?
         // idk but ill preserve its value anyway
-        data_direction = value[3];
+        data_direction = value[4];
 
         rtc.write(
             RTCParams(
                 value[1],
                 value[0],
-                value[2]
+                value[2],
+                data_direction
             )
         );
     }
@@ -54,7 +57,7 @@ final class RTCHook {
         value[0] = rtc_params.SIO;
         value[1] = rtc_params.SCK;
         value[2] = rtc_params.CS;
-        value[3] = data_direction;
+        value[4] = data_direction;
         return value;
     }
 }
@@ -63,6 +66,7 @@ final class RTC_S_35199A01 {
     bool SCK;
     bool SIO;
     bool CS;
+    bool SIO_direction;
 
     ubyte serial_data;
     int   serial_index;
@@ -71,6 +75,8 @@ final class RTC_S_35199A01 {
 
     int current_command_index;
     int current_register_index;
+
+    bool twenty_four_hour_mode;
 
     enum State {
         WAITING_FOR_COMMAND,
@@ -145,12 +151,18 @@ final class RTC_S_35199A01 {
             CommandData([]),
             CommandData([]),
         ];
+        
+        twenty_four_hour_mode = true;
     }
 
     void update_status_register_1(ubyte value) {
+        log_rtc("updating status register 1: %x", value);
         if (value & 1) reset();
 
         status_register_1.value = value & ~1;
+
+        status_register_1.value &= ~2;
+        status_register_1.value |= 1 << twenty_four_hour_mode;
     }
 
     void update_status_register_2(ubyte value) {
@@ -163,7 +175,7 @@ final class RTC_S_35199A01 {
         bool old_CS  = this.CS;
 
         SCK = rtc_params.SCK;
-        SIO = rtc_params.SIO;
+        if (rtc_params.SIO_direction) SIO = rtc_params.SIO;
         CS  = rtc_params.CS;
     
         if (rising_edge(old_CS, CS)) {
@@ -175,10 +187,9 @@ final class RTC_S_35199A01 {
         }
 
         if (falling_edge(old_SCK, SCK) && state != State.WAITING_FOR_COMMAND) {
-            
             switch (state) {
                 case State.READING_PARAMETERS:
-                    SIO = bit(this.get_active_register().value, this.serial_index); 
+                    if (!rtc_params.SIO_direction) SIO = bit(this.get_active_register().value, this.serial_index);
                     serial_index++;
 
                     if (this.serial_index == 8) {
@@ -312,7 +323,8 @@ final class RTC_S_35199A01 {
         return RTCParams(
             SCK,
             SIO,
-            CS
+            CS,
+            SIO_direction
         );
     }
 }
