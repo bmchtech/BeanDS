4a515496e7d087700907e374e30c96697e1eeab2
redthing1 <redthing1@alt.icu>
2022-11-13 23:04:50 -0800

    improve tab contents layout

--- source/ui/reng/dsdebugger.d
+++ source/ui/reng/dsdebugger.d
@@ -210,44 +210,49 @@ class DSDebuggerUIRoot : Component, Renderable2D, Updatable {
 
             mixin(GEN_NK_TABS!(Panel1Tab)("panel1_tab"));
 
-            nk_layout_row_dynamic(ctx, UI_PAD, 1);
+            auto curr_win_space = nk_window_get_content_region_size(ctx);
+            nk_layout_row_dynamic(ctx, curr_win_space.y - 42, 1);
 
-            switch (panel1_tab) {
-            case Panel1Tab.Tab1:
-                nk_layout_row_static(ctx, 30, 80, 1);
-                if (nk_button_label(ctx, "button"))
-                    TraceLog(TraceLogLevel.LOG_INFO, "button pressed");
+            if (nk_group_begin(ctx, "dashboard", nk_panel_flags.NK_WINDOW_BORDER)) {
+                nk_layout_row_dynamic(ctx, UI_PAD, 1);
+                switch (panel1_tab) {
+                case Panel1Tab.Tab1:
+                    nk_layout_row_static(ctx, 30, 80, 1);
+                    if (nk_button_label(ctx, "button"))
+                        TraceLog(TraceLogLevel.LOG_INFO, "button pressed");
 
-                nk_layout_row_dynamic(ctx, 30, 2);
-                if (nk_option_label(ctx, "easy", diff_opt == Difficulty.Easy))
-                    diff_opt = Difficulty.Easy;
-                if (nk_option_label(ctx, "hard", diff_opt == Difficulty.Hard))
-                    diff_opt = Difficulty.Hard;
-                break;
-            case Panel1Tab.Tab2:
-                nk_layout_row_dynamic(ctx, 25, 1);
-                nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-                nk_layout_row_dynamic(ctx, 20, 1);
-                nk_label(ctx, "background:", nk_text_alignment.NK_TEXT_LEFT);
-                nk_layout_row_dynamic(ctx, 25, 1);
-                if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx), 400))) {
-                    nk_layout_row_dynamic(ctx, 120, 1);
-                    bg = nk_color_picker(ctx, bg, nk_color_format.NK_RGBA);
+                    nk_layout_row_dynamic(ctx, 30, 2);
+                    if (nk_option_label(ctx, "easy", diff_opt == Difficulty.Easy))
+                        diff_opt = Difficulty.Easy;
+                    if (nk_option_label(ctx, "hard", diff_opt == Difficulty.Hard))
+                        diff_opt = Difficulty.Hard;
+                    break;
+                case Panel1Tab.Tab2:
                     nk_layout_row_dynamic(ctx, 25, 1);
-                    bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f, 0.005f);
-                    bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f, 0.005f);
-                    bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f, 0.005f);
-                    bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f, 0.005f);
-                    nk_combo_end(ctx);
+                    nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+
+                    nk_layout_row_dynamic(ctx, 20, 1);
+                    nk_label(ctx, "background:", nk_text_alignment.NK_TEXT_LEFT);
+                    nk_layout_row_dynamic(ctx, 25, 1);
+                    if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx), 400))) {
+                        nk_layout_row_dynamic(ctx, 120, 1);
+                        bg = nk_color_picker(ctx, bg, nk_color_format.NK_RGBA);
+                        nk_layout_row_dynamic(ctx, 25, 1);
+                        bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f, 0.005f);
+                        bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f, 0.005f);
+                        bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f, 0.005f);
+                        bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f, 0.005f);
+                        nk_combo_end(ctx);
+                    }
+                    break;
+                case Panel1Tab.Tab3:
+                    nk_layout_row_dynamic(ctx, 30, 1);
+                    nk_label(ctx, "Tab 3", nk_text_alignment.NK_TEXT_LEFT);
+                    break;
+                default:
+                    break;
                 }
-                break;
-            case Panel1Tab.Tab3:
-                nk_layout_row_dynamic(ctx, 30, 1);
-                nk_label(ctx, "Tab 3", nk_text_alignment.NK_TEXT_LEFT);
-                break;
-            default:
-                break;
+                nk_group_end(ctx);
             }
         }
 
