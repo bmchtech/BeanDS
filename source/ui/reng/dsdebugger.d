bae9a6511fe25988edaf706235203cf6a044b987
redthing1 <redthing1@alt.icu>
2022-11-13 19:32:22 -0800

    tweak demo ui and add tabs

--- source/ui/reng/dsdebugger.d
+++ source/ui/reng/dsdebugger.d
@@ -9,16 +9,16 @@ import re.util.interop;
 
 import raylib;
 import raylib_nuklear;
+import nuklear_ext;
 
 import std.array;
 import std.conv;
 import std.string;
 
 import ui.reng.dsvideo;
+import ui.reng.nuklear_style;
 
-// font size
-enum UI_FS = 16;
-enum UI_PAD = 4;
+enum UI_FS = 16; // font size
 
 class DSDebuggerUIRoot : Component, Renderable2D, Updatable {
     mixin Reflect;
@@ -40,6 +40,7 @@ class DSDebuggerUIRoot : Component, Renderable2D, Updatable {
         auto ui_font = raylib.LoadFontEx("./res/SourceSansPro-Regular.ttf", UI_FS, null, 0);
         ctx = InitNuklearEx(ui_font, UI_FS);
         SetNuklearScaling(ctx, cast(int) Core.window.scale_dpi);
+        apply_style(ctx);
 
         // nk_color[nk_style_colors.NK_COLOR_COUNT] table;
         // table[nk_style_colors.NK_COLOR_TEXT] = nk_rgba(190, 190, 190, 255);
@@ -81,11 +82,16 @@ class DSDebuggerUIRoot : Component, Renderable2D, Updatable {
         return status_text = val;
     }
 
+    enum Panel1Tab {
+        Tab1,
+        Tab2,
+        Tab3,
+    }
+
     private string status_text = "";
-    int active_tab = 0;
-    bool tab_picker_open = false;
     Rectangle panel1_bounds;
     Rectangle panel2_bounds;
+    Panel1Tab panel1_tab = Panel1Tab.Tab1;
 
     void update() {
         // keyboard shortcuts
@@ -109,36 +115,56 @@ class DSDebuggerUIRoot : Component, Renderable2D, Updatable {
         // GUI
         if (nk_begin(ctx, "panel 1", RectangleToNuklear(ctx, panel1_bounds),
                 nk_panel_flags.NK_WINDOW_BORDER | nk_panel_flags.NK_WINDOW_TITLE)) {
-            enum EASY = 0;
-            enum HARD = 1;
-            static int op = EASY;
-            static int property = 20;
-            nk_layout_row_static(ctx, 30, 80, 1);
-            if (nk_button_label(ctx, "button"))
-                TraceLog(TraceLogLevel.LOG_INFO, "button pressed");
-
-            nk_layout_row_dynamic(ctx, 30, 2);
-            if (nk_option_label(ctx, "easy", op == EASY))
-                op = EASY;
-            if (nk_option_label(ctx, "hard", op == HARD))
-                op = HARD;
-
-            nk_layout_row_dynamic(ctx, 25, 1);
-            nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-            nk_layout_row_dynamic(ctx, 20, 1);
-            nk_label(ctx, "background:", nk_text_alignment.NK_TEXT_LEFT);
-            nk_layout_row_dynamic(ctx, 25, 1);
-            if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx), 400))) {
-                nk_layout_row_dynamic(ctx, 120, 1);
-                bg = nk_color_picker(ctx, bg, nk_color_format.NK_RGBA);
-                nk_layout_row_dynamic(ctx, 25, 1);
-                bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f, 0.005f);
-                bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f, 0.005f);
-                bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f, 0.005f);
-                bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f, 0.005f);
-                nk_combo_end(ctx);
+            enum Difficulty {
+                Easy,
+                Hard,
             }
+
+            auto diff_opt = Difficulty.Easy;
+            auto property = 20;
+
+            nk_layout_row_begin(ctx, nk_layout_format.NK_STATIC, 30, 2);
+            if (nk_tab(ctx, Panel1Tab.Tab1.to!string.c_str, panel1_tab == Panel1Tab.Tab1)) {
+                panel1_tab = Panel1Tab.Tab1;
+            }
+            if (nk_tab(ctx, Panel1Tab.Tab2.to!string.c_str, panel1_tab == Panel1Tab.Tab2)) {
+                panel1_tab = Panel1Tab.Tab2;
+            }
+
+            nk_layout_row_dynamic(ctx, UI_PAD, 1);
+
+            switch (panel1_tab) {
+            case Panel1Tab.Tab1:
+                nk_layout_row_static(ctx, 30, 80, 1);
+                if (nk_button_label(ctx, "button"))
+                    TraceLog(TraceLogLevel.LOG_INFO, "button pressed");
+
+                nk_layout_row_dynamic(ctx, 30, 2);
+                if (nk_option_label(ctx, "easy", diff_opt == Difficulty.Easy))
+                    diff_opt = Difficulty.Easy;
+                if (nk_option_label(ctx, "hard", diff_opt == Difficulty.Hard))
+                    diff_opt = Difficulty.Hard;
+                break;
+            default:
+                nk_layout_row_dynamic(ctx, 25, 1);
+                nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+
+                nk_layout_row_dynamic(ctx, 20, 1);
+                nk_label(ctx, "background:", nk_text_alignment.NK_TEXT_LEFT);
+                nk_layout_row_dynamic(ctx, 25, 1);
+                if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx), 400))) {
+                    nk_layout_row_dynamic(ctx, 120, 1);
+                    bg = nk_color_picker(ctx, bg, nk_color_format.NK_RGBA);
+                    nk_layout_row_dynamic(ctx, 25, 1);
+                    bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f, 0.005f);
+                    bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f, 0.005f);
+                    bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f, 0.005f);
+                    bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f, 0.005f);
+                    nk_combo_end(ctx);
+                }
+                break;
+            }
+
         }
 
         nk_end(ctx);
